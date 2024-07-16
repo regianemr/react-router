d5658fc381711e3e0d566c2f1377acaddb6128d1
Regiane <regianemelo1995@gmail.com>
2024-07-16 15:22:01 -0300

    rota dinamica

---
+++ router/src/hooks/useFetch.jsx
@@ -0,0 +1,82 @@
+import { useState, useEffect } from "react";
+
+// 4 - custom hook
+export const useFetch = (url) => {
+
+    const [data, setData] = useState(null)
+
+    // 5 - refatorando post
+    const [config, setConfig] = useState(null)
+    const [method, setMethod] = useState(null)
+    const [callFetch, setCallFetch] = useState(null)
+
+    // 6 - loading
+    const [loading, setLoading] = useState(false)
+
+    // 7 - erros
+    const [error, setError] = useState(null)
+
+    const httpConfig = (data, method) => {
+
+        if(method ==="POST") {
+            setConfig({
+                method,
+                headers: {
+                    "content-type": "application/json"
+                },
+                body: JSON.stringify(data)
+            })
+
+            setMethod(method)
+        }
+    }
+
+    useEffect(() => {
+        // 7 - tratando erros
+        const fetchData = async () => {
+            try {
+                // 6 - loading
+                setLoading(true)
+
+                const res = await fetch(url)
+                const json = await res.json()
+
+                setData(json)
+            } catch (error) {
+                console.log(error.message)
+
+                setError("Houve algum erro ao carregar os dados")
+            }
+            setLoading(false)
+        }
+
+        fetchData()
+    }, [url, callFetch])
+
+    // 5 - refatorando post
+    useEffect(() => {
+
+        const httpRequest = async() => {
+            let json
+
+            if(method === "POST") {
+                // 6 - loading
+                setLoading(true)
+
+                let fetchOptions = [url, config]
+
+                const res = await fetch(...fetchOptions)
+
+                json = await res.json()
+
+                // 6 - loading
+                setLoading(false)
+            }
+
+            setCallFetch(json)
+        }
+        httpRequest()
+    }, [config, method, url])
+
+    return { data, httpConfig, loading, error }
+}
--- router/src/main.jsx
+++ router/src/main.jsx
@@ -5,18 +5,52 @@ import './index.css'
 
 import Contact from "./routes/Contact"
 
+// 2 - página de erro
+import ErrorPage from './routes/ErrorPage.jsx'
+
+// 1 - configurando o router
 import { createBrowserRouter, RouterProvider, Router } from "react-router-dom"
 
+// 3 - componente base
+import Home from './routes/Home.jsx'
+
+// 7 - Rota dinamica
+import Product from './routes/Product.jsx'
+
+// 8 - nested route
+import Info from './routes/Info.jsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />
+    element: <App />,
+    errorElement: <ErrorPage />,
+    // 3 - componente base
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+      // 7 - rota dinamica
+      {
+        path: "products/:id",
+        element: <Product />,
+      },
+      // 8- nested route (rota aninhada)
+      {
+        path: "products/:id/info",
+        element: <Info />,
+      }
+    ]
   },
-  {
-    path: "contact",
-    element: <Contact />,
-  },
+  // {
+  //   path: "contact",
+  //   element: <Contact />,
+  // },
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
